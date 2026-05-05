fcb7148277d6d1012f3850c7b1ddefe6614658f6
Martin Matovu <martin@matovius.dev>
2026-05-05 21:04:33 +0300

    feat: update homepage
    - add more contextual information
    - add more experiments
    - add page footer
    - add slight style changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,26 +8,57 @@
 
 <main>
 	<div class="container">
-		<h1>Welcome to the Laboratory of Martin Matovu</h1>
-		<p>
-			Here, I conduct experiments in Svelte. Do take a gander! You'll note that stuff is quite
-			unpolished around here. This zone is for pure experimentation, and not for aesthetics.
-		</p>
+		<hgroup>
+			<h1>Welcome to the Laboratory of Martin Matovu</h1>
+			<p>
+				Here, I conduct experiments in Svelte. You'll note that stuff is quite unpolished around
+				here. This website is for pure experimentation, and not for aesthetics.
+			</p>
+			<p>
+				Each experiment has a set of notes I wrote in the process of building it. You can see these
+				notes by clicking a button in the bottom-right corner of the window.
+			</p>
+		</hgroup>
 
 		<ul>
 			<li>
 				<a href={resolve('/video-canvas')}>Video Canvas</a>
-				<p>
-					On rendering videos inside the HTML Canvas. You could call it a canvas-based video payer.
-				</p>
+				<p>On rendering videos inside the HTML Canvas.</p>
+			</li>
+			<li>
+				<a href={resolve('/svg-text-path')}>SVG Text Path</a>
+				<p>On drawing text along an svg path.</p>
+			</li>
+			<li>
+				<a href={resolve('/fullscreen-menu')}>Fullscreen Menu</a>
+				<p>On displaying a fullscreen dropdown menu.</p>
 			</li>
 		</ul>
 	</div>
 </main>
 
+<footer>
+	<small class="copyright-statement">
+		A <a href="https://matovius.dev" target="_blank" rel="noopener noreferrer" class="link"
+			>@matovius</a
+		>
+		project. Check out the
+		<a href="https://github.com/matovius/lab" target="_blank" rel="noopener noreferrer" class="link"
+			>GitHub</a
+		>.
+	</small>
+	<small class="disclaimer">
+		Disclaimer: I DO NOT use generative AI in any of my work. It is a personal choice based on what
+		I feel about current implementations of the technology and the fact that I enjoy writing code
+		myself. Each experiment is a combination of wracking my brain and using accessible web resources
+		and tutorials.
+	</small>
+</footer>
+
 <style>
 	main {
 		padding-inline: 1.25rem;
+		padding-block-end: 5rem;
 
 		.container {
 			width: 100%;
@@ -36,15 +67,16 @@
 		}
 	}
 
-	h1 {
-		letter-spacing: -3px;
-		margin-block-start: 1em;
-		text-wrap: balance;
-
-		+ p {
-			font-size: var(--text-md);
-			font-weight: 600;
-			margin-block: 2em;
+	hgroup {
+		h1 {
+			letter-spacing: -3px;
+			margin-block-start: 0.5em;
+			text-wrap: balance;
+		}
+		p {
+			/*font-size: var(--text-md);*/
+			/*font-weight: 600;*/
+			margin-block-start: 1em;
 			color: var(--color-neutral-600);
 		}
 	}
@@ -53,6 +85,7 @@
 		list-style: none;
 		display: grid;
 		gap: 2rem;
+		padding-block-start: 2.5rem;
 
 		li {
 			display: grid;
@@ -65,6 +98,21 @@
 			&:hover p {
 				color: var(--color-neutral-800);
 			}
+		}
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 1.25rem;
+		padding: 1.25rem;
+
+		small.disclaimer {
+			color: var(--color-neutral-500);
+			text-align: center;
+			max-width: 1200px;
 		}
 	}
 </style>
